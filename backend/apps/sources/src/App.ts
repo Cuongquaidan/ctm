a3a1b1166049e25e94bac9e6a99ee574e3ee1e13
Hà Mạnh Cường test <hamanhcuong18112004@gmail.com>
2025-12-23 06:02:18 +0700

    u

--- backend/apps/sources/src/App.ts
+++ backend/apps/sources/src/App.ts
@@ -1,55 +1,61 @@
-import Fastify from 'fastify';
-import { FastifyRequest, FastifyReply } from 'fastify';
-import routeAdmins from '../modules/admins/routes.js';
-import routeCustomers from '../modules/customers/routes.js';
-import routesSites from '../modules/sites/routes.js';
-import multipart from '@fastify/multipart';
-import fastifyStatic from '@fastify/static';
-import formbody from '@fastify/formbody';
-import cookie from '@fastify/cookie';
-import session from '@fastify/session';
-import path from 'path';
-import cors from '@fastify/cors';
-import Route from './Route.js';
-import { Request } from './Request.js';
-import mysqlPlugin from './plugins/mysql.plugin.js';
-import modelsPlugin from './plugins/models.plugin.js';
+import Fastify from "fastify";
+import { FastifyRequest, FastifyReply } from "fastify";
+import routeAdmins from "../modules/admins/routes.js";
+import routeCustomers from "../modules/customers/routes.js";
+import routesSites from "../modules/sites/routes.js";
+import multipart from "@fastify/multipart";
+import fastifyStatic from "@fastify/static";
+import formbody from "@fastify/formbody";
+import cookie from "@fastify/cookie";
+import session from "@fastify/session";
+import path from "path";
+import cors from "@fastify/cors";
+import Route from "./Route.js";
+import { Request } from "./Request.js";
+import mysqlPlugin from "./plugins/mysql.plugin.js";
+import modelsPlugin from "./plugins/models.plugin.js";
 class App {
     static async run() {
         const fastifyServer = Fastify({
-            logger: true
+            logger: true,
         });
         await fastifyServer.register(cors, {
-            "origin": process.env.APP_URL ? process.env.APP_URL.split(',') : "*",
-            "credentials": true
+            origin: process.env.APP_URL?.split(",") || [],
+            credentials: true,
         });
         await fastifyServer.register(cookie, {
-            secret: 'u4V$eP7#r1!q@Zx2M%tB^y9L&g0D*hN8sF+Wc3J=Rj?oK5pT|i>H<lA_Cvb6m',
+            secret: "u4V$eP7#r1!q@Zx2M%tB^y9L&g0D*hN8sF+Wc3J=Rj?oK5pT|i>H<lA_Cvb6m",
             parseOptions: {},
         });
         await fastifyServer.register(session, {
-            secret: 'N5$zM!3wLkY#x9h&Q8fT^2u@oG7rV*jC4D=pJ?e%S1aB|iZ6H>R<yU_E+0nWvt',
+            secret: "N5$zM!3wLkY#x9h&Q8fT^2u@oG7rV*jC4D=pJ?e%S1aB|iZ6H>R<yU_E+0nWvt",
             cookie: {
                 httpOnly: true,
                 sameSite: "lax",
                 secure: false,
-                path: '/',
+                path: "/",
             },
             saveUninitialized: false,
-            rolling: true
+            rolling: true,
         });
         await fastifyServer.register(mysqlPlugin);
         await fastifyServer.register(modelsPlugin);
         // console.log(fastifyServer.models);
-        await fastifyServer.register(multipart, { limits: { fileSize: 20 * 1024 * 1024 }, attachFieldsToBody: false });
+        await fastifyServer.register(multipart, {
+            limits: { fileSize: 20 * 1024 * 1024 },
+            attachFieldsToBody: false,
+        });
         await fastifyServer.register(fastifyStatic, {
-            root: path.join(path.resolve(), 'public'),
-            prefix: '/',
+            root: path.join(path.resolve(), "public"),
+            prefix: "/",
         });
         await fastifyServer.register(formbody);
 
-
-        for await (const router of [...routeAdmins, ...routeCustomers, ...routesSites]) {
+        for await (const router of [
+            ...routeAdmins,
+            ...routeCustomers,
+            ...routesSites,
+        ]) {
             const method: string = Route.getMethod(router);
             fastifyServer.route({
                 url: router.link,
@@ -57,14 +63,24 @@ class App {
                 handler: async (req: FastifyRequest, reply: FastifyReply) => {
                     try {
                         const mysqlPool = (fastifyServer as any).mysql;
-                        const classController = new router.controller(req, reply, fastifyServer, mysqlPool);
+                        const classController = new router.controller(
+                            req,
+                            reply,
+                            fastifyServer,
+                            mysqlPool
+                        );
                         if (classController[router.action] === undefined) {
                             throw new Error(`Cannot method ${router.action}`);
                         }
-                        const code: number = (req.params as { code: number }).code;
+                        const code: number = (req.params as { code: number })
+                            .code;
                         // const mysqlPool = (fastifyServer as any).mysql;
 
-                        classController.request = new Request(req, reply, mysqlPool);
+                        classController.request = new Request(
+                            req,
+                            reply,
+                            mysqlPool
+                        );
                         // classController.db = new Transaction(fastifyServer); // <-- truyền MySQL pool
                         // BaseModel.setupFastify(fastifyServer);
                         // BaseModel.setFastify(app);
@@ -84,21 +100,26 @@ class App {
                         await classController.request.end();
                         return;
                     } catch (error: any) {
-                        return reply.code(400).send({ msg: error.message, status: 400 });
+                        return reply
+                            .code(400)
+                            .send({ msg: error.message, status: 400 });
                     }
-                }
+                },
             });
         }
         fastifyServer.setNotFoundHandler(async (request, reply) => {
             return reply.code(404).send({
                 msg: `Cannot find ${request.method} ${request.url}`,
-                status: 404
+                status: 404,
             });
         });
         const start = async () => {
             try {
-                await fastifyServer.listen({ port: Number(process.env.PORT || 3000), host: '0.0.0.0' });
-                console.log('Start server success at port 3000');
+                await fastifyServer.listen({
+                    port: Number(process.env.PORT || 3000),
+                    host: "0.0.0.0",
+                });
+                console.log("Start server success at port 3000");
             } catch (err) {
                 fastifyServer.log.error(err);
                 process.exit(1);
